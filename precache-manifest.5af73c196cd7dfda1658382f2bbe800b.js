93e36c66e1f29203aa5487deb92f6c7dc80f1352
Kontent <czepiel.marcin@outlook.com>
2021-03-17 17:14:35 +0100

    add files

---
+++ precache-manifest.5af73c196cd7dfda1658382f2bbe800b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e067b3ae5de8d25ed6f2de67a8489eb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a8a3d899f25e77832c",
+    "url": "/static/css/main.657b1ed4.chunk.css"
+  },
+  {
+    "revision": "73fcf04c19197259bd81",
+    "url": "/static/js/2.a2604492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a2604492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a8a3d899f25e77832c",
+    "url": "/static/js/main.f5ba9011.chunk.js"
+  },
+  {
+    "revision": "53bf7c65b3e97357c183",
+    "url": "/static/js/runtime-main.49ef3424.js"
+  }
+]);
